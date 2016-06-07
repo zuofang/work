3d1da93bd7118ec2a1736e21d1ea2f14ac313822
zuofang <943032656@qq.com>
2016-06-07 18:04:23 +0800

    ruiyin-agent

---
+++ ruiyin-agent/js/api.js
@@ -0,0 +1,48 @@
+window.realtyApi={
+	//获取房产类型
+	getRealtyTypeList:function(callBacck){
+		mui.ajax('http://139.196.232.30/apis/realty/realty-types/',{
+			dataType:'json',
+			timeout:10000,
+			headers:{'Authorization','Token d61cb799e4114300c21aabd4ba346dd2ee98649c'},
+			success:function(data){
+				callBack(data);
+			},
+			error:function(){
+				mui.toast('服务器繁忙');	
+			}
+		});
+	},
+	
+	//获取周边类型
+	getFacilityTypeList:function(callBack){
+		mui.ajax('http://139.196.232.30/apis/realty/facility-types/',{
+			dataType:'json',
+			timeout:10000,
+			headers:{'Authorization','Token d61cb799e4114300c21aabd4ba346dd2ee98649c'},
+			success:function(data){
+				callBack(data);
+			},
+			error:function(){
+				mui.toast('服务器繁忙');	
+			}
+		});
+	},
+	
+	//上传房产
+	postRealty:function(data,callBack){
+		mui.ajax('http://139.196.232.30/apis/realty/realties/',{
+			data:data,
+			dataType:'json',
+			type:'POST',
+			timeout:10000,
+			headers:{'Authorization','Token d61cb799e4114300c21aabd4ba346dd2ee98649c'},
+			success:function(data){
+				callBack(data);
+			},
+			error:function(){
+				mui.toast('服务器繁忙');	
+			}
+		});
+	}
+}
