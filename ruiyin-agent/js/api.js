8165f725cb4648fa2d7505ef60529e08dfc05c1a
zuofang <943032656@qq.com>
2016-06-11 21:32:23 +0800

    添加房地产

--- ruiyin-agent/js/api.js
+++ ruiyin-agent/js/api.js
@@ -1,4 +1,33 @@
 window.realtyApi={
+	//获取验证码
+	getPinCode:function(country,phone,callBack){
+		var data={
+			"country":country,
+			"phone":phone
+		};
+		mui.post("http://139.196.232.30/apis/sms/pin/",data,function(data){
+			callBack(data);		
+		},'json');
+	},
+	
+	//注册
+	//type:1:agent 2:client
+	register:function(data,callBack){			
+		mui.post("http://139.196.232.30/apis/rest-auth/registration/",data,function(data){
+				callBack(data);		
+		},'json');
+	},
+	
+	//登录
+	login:function(userName,password,callBack){
+		var data={
+			"username":userName,
+			"password":password
+		};
+		mui.post("http://139.196.232.30/apis/rest-auth/login/",data,function(data){
+			callBack(data);
+		})
+	},
 	//获取房产类型
 	getRealtyTypeList:function(callBack){
 		mui.ajax('http://139.196.232.30/apis/realty/realty-types/',{
@@ -31,18 +60,41 @@ window.realtyApi={
 	
 	//上传房产
 	postRealty:function(data,callBack){
-		mui.ajax('http://139.196.232.30/apis/realty/realties/',{
-			data:data,
-			dataType:'json',
-			type:'POST',
-			timeout:10000,
-			headers:{'Authorization':'Token d61cb799e4114300c21aabd4ba346dd2ee98649c'},
-			success:function(data){
-				callBack(data);
-			},
-			error:function(){
+		$.ajax({
+		     type: 'POST',
+		    contentType: "application/json",
+		     url: 'http://139.196.232.30/apis/realty/realties/' ,
+		     headers:{'Authorization':'Token d61cb799e4114300c21aabd4ba346dd2ee98649c'},
+		     data:JSON.stringify(data),
+		     success: function(data){
+				 callBack(data);
+			 },
+			 error:function(){
 				mui.toast('服务器繁忙');	
-			}
+			},
+		    dataType: 'json'
+		});
+	},
+	
+	//上传图片
+	uploadImg:function(url,file,callBack){
+		$.ajax({
+		     type: 'POST',
+		     contentType: "application/json",
+		     url: url,
+		     headers:{'Authorization':'Token d61cb799e4114300c21aabd4ba346dd2ee98649c'},
+		     data:{
+		     	"file":file
+		     },
+		     success: function(data){
+				 callBack(data);
+			 },
+			 error: function(XMLHttpRequest, textStatus, errorThrown) {
+				 alert(XMLHttpRequest.status);
+				 alert(XMLHttpRequest.readyState);
+				 alert(textStatus);
+   			 },
+		    dataType: 'json'
 		});
 	}
 };
