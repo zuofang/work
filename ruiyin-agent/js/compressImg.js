750ca6b1dabb619cbd8912ca761851071b8443fe
zuofang <943032656@qq.com>
2016-07-15 15:53:57 +0800

    服务端 第一版

--- ruiyin-agent/js/compressImg.js
+++ ruiyin-agent/js/compressImg.js
@@ -1,6 +1,31 @@
 (function($){
+	$.compressImg=function(path,maxWidth,maxHeight,quality,callBack){
+		var img=new Image();
+		img.src=path;
+		img.onload=function(){
+			var imgWidth,imgHeight;
+			var canvas=document.createElement('canvas');
+			if(img.width>img.height){
+				imgWidth=maxWidth;
+				imgHeight=maxHeight*(img.height/img.width);
+			}else if(img.width<img.height){
+				imgHeight=maxHeight;
+				imgWidth=maxWidth*(img.width/img.height);
+			}else{
+				imgWidth=maxWidth;
+				imgHeight=maxHeight;
+			}
+			canvas.height=imgHeight;
+			canvas.width=imgWidth;
+			var con=canvas.getContext('2d');
+			con.clearRect(0,0,canvas.width,canvas.height);
+			con.drawImage(img,0,0,imgWidth,imgHeight);
+			var base64=canvas.toDataURL('image/jpeg',quality);
+			callBack(base64);
+		}
+	};
 	$.fn.extend({
-		compressImg:function(obj){
+		compressImgs:function(obj){
 			for(var i in obj.images){
 				(function(file,maxWidth,maxHeight,quality,obj){
 					var img=new Image();
@@ -30,8 +55,7 @@
 			    		'</li>');
 					}
 				}(obj.images[i],obj.maxWidth,obj.maxHeight,obj.quality,$(this)))
-				}
 			}
-
+		}
 	});
 })(jQuery)
